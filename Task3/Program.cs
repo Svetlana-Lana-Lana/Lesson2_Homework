ef4296c5f3b5135aa7885cd552226c755e229919
Svetlana <Svetlanat1005@yandex.ru>
2024-02-20 18:50:49 +0300

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int num = 72;
+int first = num/10;
+int second = num%10;
+if(num > 10 && num < 99){
+    if(first>=second){
+        Console.Write(first);
+    }
+    else{
+        Console.Write(second);
+    }
+}

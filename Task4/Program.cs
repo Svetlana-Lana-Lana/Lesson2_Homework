bc1c7be88caed9d30ec2c6484ebeee4e1ea6ddab
Svetlana <Svetlanat1005@yandex.ru>
2024-02-23 15:46:18 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿int n = 568;
+int num = n;
+int number = n;
+int count = 0;
+int tens = 1;
+int result = 0;
+
+
+
+
+while (num > 0)
+{
+    num = num / 10;
+    count++;
+    if (num > 0)
+    {
+        tens = tens * 10;
+    }
+}
+while (count > 0)
+{
+    result = number / tens;
+    number = number % tens;
+    tens = tens / 10;
+    count = count - 1;
+    Console.Write(result + ", ");
+}
+
+
+
+
+
